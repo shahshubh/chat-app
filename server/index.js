57c9dddc4fa6a806850850f3e67327e55d07d1c7
shubh <shahshubh1010@gmail.com>
2020-04-10 12:36:50 +0530

    done socket user connect and disconnect

--- server/index.js
+++ server/index.js
@@ -4,9 +4,26 @@ const http = require("http");
 
 const PORT = process.env.PORT || 3001
 
+const router = require('./router');
+
 const app = express();
 const server =http.createServer(app);
 const io = socketio(server);
 
+io.on("connection",(socket) => {
+    console.log("We have New connection !!");
+
+    socket.on('join', ({ name, room }, callback) => {
+        console.log(name,room);
+
+        
+    });
+
+    socket.on("disconnect", () => {
+        console.log("User LEFT !!");
+    });
+});
+
+app.use(router);
 
 server.listen(PORT, () => console.log(`Server running at port ${PORT}`));
